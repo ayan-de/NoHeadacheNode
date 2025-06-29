5cf19b4463566772ff07d6fac6ac1efc73657fdb
Ayan De <deayan252@gmail.com>
2025-06-29 17:57:49 +0530

    "example added"

--- example/main.js
+++ example/main.js
@@ -21,31 +21,31 @@ const loadedRoutes = loadRoutes(routesPath);
 registerRoutes(app, loadedRoutes);
 
 // 1. Add middleware
-// app.use((req, res, next) => {
-//   console.log(`Request: ${req.method} ${req.url}`);
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
+});
 
 // 2. Add routes
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome!" });
-// });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome!" });
+});
 
-// app.get("/users/:id", (req, res) => {
-//   const { id } = req.params;
-//   const { include } = req.query;
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { include } = req.query;
 
-//   res.json({
-//     user: { id, name: `User ${id}` },
-//     include: include,
-//   });
-// });
+  res.json({
+    user: { id, name: `User ${id}` },
+    include: include,
+  });
+});
 
-// app.post("/users", (req, res) => {
-//   const userData = req.body;
-//   // Save to database...
-//   res.status(201).json({ id: Date.now(), ...userData });
-// });
+app.post("/users", (req, res) => {
+  const userData = req.body;
+  // Save to database...
+  res.status(201).json({ id: Date.now(), ...userData });
+});
 
 // 3. Start server
 app.listen(3000, () => {
